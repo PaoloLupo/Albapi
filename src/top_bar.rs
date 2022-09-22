66933750c0793dbff8e9ede3b4c9cb29539b9fd9
Paolo Guillen Lupo <paologuillenlupo@gmail.com>
2022-09-21 19:52:23 -0500

    coloque un saludo

--- src/top_bar.rs
+++ src/top_bar.rs
@@ -1,16 +1,17 @@
 use crate::Alba;
 use eframe::egui;
 
-impl Alba{
-    pub fn top_bar(&mut self, ctx:&egui::Context, frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui|{
-            egui::menu::bar(ui, |ui|{
-                ui.menu_button("File", |ui|{
-                    if ui.button("Quit").clicked(){
+impl Alba {
+    pub fn top_bar(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
                         frame.close();
                     }
                 })
             });
         });
     }
+    println!("Hello, world!");
 }
